ebdfb89119fcb294ab0ae46a5c9467231eb6112b
Pijush Dey <pijushdey346@gmail.com>
2022-11-03 13:59:51 +0530

    Added the SQL database

--- WebsiteDatabase.sql
+++ WebsiteDatabase.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `websitedata` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `websitedata`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: websitedata
@@ -419,4 +421,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-02 22:39:01
+-- Dump completed on 2022-11-03 13:55:34
